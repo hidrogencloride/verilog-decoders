6376c85dc8a84203fe171a6f2a80d179dc2d30ea
Bryan Pesquera <vryonette@gmail.com>
2017-03-25 01:11:43 -0400

    Create decoder3-8.v

---
+++ decoder3-8.v
@@ -0,0 +1,23 @@
+/*
+Authors: Bryan Pesquera
+Description: Decoder from 3 to 8, design simulated 
+using Icarus Verilog 0.10.0 11/23/14 on edaplayground.com
+Date: Feb. 15, 2017.
+*/
+
+module decoder3-8(din, en, dout);
+  input din [1:0];
+  input en;
+  output[7:0] dout;
+  
+  //all combinations for 3 inputs to 8 outputs
+  assign dout[0] = (~din[0] & ~din[1] & ~en);
+  assign dout[1] = (~din[0] & ~din[1] & en);
+  assign dout[2] = (~din[0] & din[1] & ~en);
+  assign dout[3] = (~din[0] & din[1] & en);
+  assign dout[4] = (din[0] & ~din[1] & ~en);
+  assign dout[5] = (din[0] & ~din[1] & en);
+  assign dout[6] = (din[0] & din[1] & ~en);
+  assign dout[7] = (din[0] & din[1] & en);
+  
+endmodule
